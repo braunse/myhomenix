fd484cee2bb716dd50df655923e05e190ab22668
Sebastien Braun <sebastien@sebbraun.de>
2022-08-14 12:35:42 +0200

    Refactor Neovim keymap

--- home/neovim.nix
+++ home/neovim.nix
@@ -11,6 +11,10 @@ let
     };
 in
 {
+  imports = [
+    ./neovim/keymap.nix
+  ];
+
   programs.neovim = {
     enable = true;
     viAlias = true;
@@ -43,7 +47,7 @@ in
         set termguicolors
       endif
       syntax enable
-      colorscheme night-owl
+      colorscheme everforest
 
       set splitright
       set splitbelow
@@ -52,74 +56,11 @@ in
       vnoremap ,. <Esc>
 
       let g:mapleader = ','
-      map <Leader>gd <Plug>(coc-definition)
-      map <Leader>gi <Plug>(coc-implementation)
-      map <Leader>gt <Plug>(coc-type-definition)
-      map <Leader>ah :call CocActionAsync('doHover')<cr>
-      map <Leader>en <Plug>(coc-diagnostic-next)
-      map <Leader>ep <Plug>(coc-diagnostic-prev)
-      map <Leader>fr <Plug>(coc-references)
-      map <Leader>rn <Plug>(coc-rename)
-      map <Leader>rr <Plug>(coc-refactor)
-      map <Leader>qf <Plug>(coc-fix-current)
-      map <Leader>al <Plug>(coc-codeaction-line)
-      map <Leader>ac <Plug>(coc-codeaction-cursor)
-      map <Leader>ao <Plug>(coc-codelens-action)
-
-      nnoremap <Leader>kd :<C-u>CocList diagnostics<cr>
-      nnoremap <Leader>kc :<C-u>CocList commands<cr>
-      nnoremap <Leader>ko :<C-u>CocList outline<cr>
-      nnoremap <Leader>kr :<C-u>CocListResume<cr>
-
       inoremap <silent><expr> <c-space> coc#refresh()
       inoremap <expr> <cr> pumvisible() ? "\<C-y>" : "\<CR>"
 
       autocmd CursorHold * silent call CocActionAsync('highlight')
       autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
-
-      nnoremap <Leader>tn :NERDTreeToggle<CR>
-      nnoremap <Leader>fn :NERDTreeFind<CR>
-      nnoremap <Leader>gn :NERDTreeFocus<CR>
-
-      nnoremap <silent> <A-,> <Cmd>BufferPrevious<CR>
-      nnoremap <silent> <A-.> <Cmd>BufferNext<CR>
-      nnoremap <silent> <A-;> <Cmd>BufferMovePrevious<CR>
-      nnoremap <silent> <A-:> <Cmd>BufferMoveNext<CR>
-      nnoremap <silent> <A-1> <Cmd>BufferGoto 1<CR>
-      nnoremap <silent> <A-2> <Cmd>BufferGoto 2<CR>
-      nnoremap <silent> <A-3> <Cmd>BufferGoto 3<CR>
-      nnoremap <silent> <A-4> <Cmd>BufferGoto 4<CR>
-      nnoremap <silent> <A-5> <Cmd>BufferGoto 5<CR>
-      nnoremap <silent> <A-6> <Cmd>BufferGoto 6<CR>
-      nnoremap <silent> <A-7> <Cmd>BufferGoto 7<CR>
-      nnoremap <silent> <A-8> <Cmd>BufferGoto 8<CR>
-      nnoremap <silent> <A-9> <Cmd>BufferGoto 9<CR>
-      nnoremap <silent> <A-0> <Cmd>BufferLast<CR>
-      nnoremap <silent> <A-p> <Cmd>BufferPin<CR>
-      nnoremap <silent> <A-c> <Cmd>BufferClose<CR>
-      nnoremap <silent> <A-g> <Cmd>BufferPick<CR>
-
-      nnoremap <Leader>bb <Cmd>BufferPick<CR>
-      nnoremap <Leader>b, <Cmd>BufferPrevious<CR>
-      nnoremap <Leader>b. <Cmd>BufferNext<CR>
-      nnoremap <Leader>bm, <Cmd>BufferMovePrevious<CR>
-      nnoremap <Leader>bm. <Cmd>BufferMoveNext<CR>
-      nnoremap <Leader>b1 <Cmd>BufferGoto 1<CR>
-      nnoremap <Leader>b2 <Cmd>BufferGoto 2<CR>
-      nnoremap <Leader>b3 <Cmd>BufferGoto 3<CR>
-      nnoremap <Leader>b4 <Cmd>BufferGoto 4<CR>
-      nnoremap <Leader>b5 <Cmd>BufferGoto 5<CR>
-      nnoremap <Leader>b6 <Cmd>BufferGoto 6<CR>
-      nnoremap <Leader>b7 <Cmd>BufferGoto 7<CR>
-      nnoremap <Leader>b8 <Cmd>BufferGoto 8<CR>
-      nnoremap <Leader>b9 <Cmd>BufferGoto 9<CR>
-      nnoremap <Leader>b0 <Cmd>BufferLast<CR>
-      nnoremap <Leader>bc <Cmd>BufferClose<CR>
-      nnoremap <Leader>bp <Cmd>BufferPin<CR>
-      nnoremap <Leader>bon <Cmd>BufferOrderByBufferNumber<CR>
-      nnoremap <Leader>bod <Cmd>BufferOrderByDirectory<CR>
-      nnoremap <Leader>bol <Cmd>BufferOrderByLanguage<CR>
-      nnoremap <Leader>bow <Cmd>BufferOrderByWindowNumber<CR>
     '';
     coc = {
       enable = true;
@@ -139,26 +80,24 @@ in
       };
     };
     plugins = with pkgs.vimPlugins; [
+      # Cosmetics and UI
       vim-airline
       {
         plugin = vim-airline-themes;
         config = ''
-          let g:airline_theme = "night_owl"
+          let g:airline_theme = "everforest"
         '';
       }
-      nerdtree
-      nerdcommenter
-      direnv-vim
       vim-devicons
-      ultisnips
-      vim-snippets
-      vim-startify
-      indent-blankline-nvim
-      which-key-nvim
+      vim-startify # Fancy start screen
       nvim-web-devicons
-      nvim-tree-lua
       vim-night-owl
-      coc-json
+      everforest
+
+      # File and project management
+      nerdtree
+      nvim-tree-lua
+      ctrlp
       {
         plugin = barbar-nvim;
         config = ''
@@ -167,94 +106,20 @@ in
           let bufferline.auto_hide = v:true
         '';
       }
-      {
-        plugin = which-key-nvim;
-        config = ''
-          lua << EOF
-            local wk = require("which-key")
 
-            wk.setup {
-            }
+      # Programming
+      nerdcommenter
+      coc-json
 
-            wk.register({
-              g = {
-                name = "Go to ...",
-                d = "Definition (LSP)",
-                i = "Implementation (LSP)",
-                t = "Type Definition (LSP)",
-                n = "NerdTree",
-              },
-              a = {
-                name = "Action",
-                h = "Hover (LSP)"
-              },
-              e = {
-                name = "Diagnostics",
-                n = "Next (LSP)",
-                p = "Prev (LSP)"
-              },
-              f = {
-                name = "Find",
-                r = "References (LSP)",
-                n = "NerdTree"
-              },
-              r = {
-                name = "Refactor",
-                n = "Rename (LSP)",
-                r = "Refactor (LSP)"
-              },
-              q = {
-                name = "Quick",
-                f = "Fix Current (LSP)"
-              },
-              k = {
-                name = "List",
-                d = "Diagnostics (LSP)",
-                c = "Commands (LSP)",
-                o = "Outline (LSP)",
-                r = "Resume"
-              },
-              t = {
-                name = "Toggle",
-                n = "NerdTree"
-              },
-              b = {
-                name = "Buffers",
-                b = "Pick",
-                [","] = "Previous",
-                ["."] = "Next",
-                m = {
-                  name = "Move",
-                  [","] = "Previous",
-                  ["."] = "Next",
-                },
-                ["1-9"] = "Go to buffer number _",
-                ["1"] = "which_key_ignore",
-                ["2"] = "which_key_ignore",
-                ["3"] = "which_key_ignore",
-                ["4"] = "which_key_ignore",
-                ["5"] = "which_key_ignore",
-                ["6"] = "which_key_ignore",
-                ["7"] = "which_key_ignore",
-                ["8"] = "which_key_ignore",
-                ["9"] = "which_key_ignore",
-                ["0"] = "Go to last buffer",
-                c = "Close",
-                p = "Pin",
-                o = {
-                  name = "Order by ...",
-                  n = "Number",
-                  d = "Directory",
-                  l = "Language",
-                  w = "Window Number"
-                },
-              },
-            }, {
-              prefix = "<Leader>"
-            })
-          EOF
-        '';
-      }
+      # Environment
+      direnv-vim
+
+      # Snippets
+      ultisnips
+      vim-snippets
+
+      # Utilities
+      indent-blankline-nvim
     ];
   };
 }
---
+++ home/neovim/keymap.nix
@@ -0,0 +1,149 @@
+{ config, lib, pkgs, ... }:
+with builtins;
+with lib;
+let
+  renderName = n:
+    if isList (match "[a-zA-Z]+" n)
+    then n
+    else "[\"${n}\"]";
+
+  renderGroup = i: g:
+    let
+      name = if isString (g.group or null)
+      then "name = ${renderString g.group}, \n${i}  "
+      else "";
+      mappings = removeAttrs g [ "group" ];
+      renderedMappings = concatMapStringsSep ",\n${i}  "
+        (n: "${renderName n} = ${render "${i}  " g.${n}}")
+        (attrNames mappings);
+    in
+    "{\n${i}  ${name}${renderedMappings}\n${i}}";
+
+  renderString = s:
+    "\"${replaceStrings ["\""] ["\\\""] s}\"";
+
+  renderMapping = m:
+    if isString m then renderString m else
+    let
+      output =
+        if m ? plug then "<Plug>${m.plug}"
+        else if m ? cmd then "<Cmd>${m.cmd}<CR>"
+        else if m ? to then m.to
+        else throw "Could not evaluate mapping ${generators.toJSON {} m}";
+      label = m.label or (
+        if m.hide or false then "which_key_ignore"
+        else m.plug or m.cmd or m.to
+      );
+      modeOpt = optionalString (m ? mode) ", mode = ${renderString m.mode}";
+      prefixOpt = optionalString (m ? prefix) ", prefix = ${renderString m.prefix}";
+      buffer = optionalString (m ? buffer) ", buffer = ${toString m.buffer}";
+      silent = optionalString (m ? silent) ", silent = ${toString m.silent}";
+      noremap = optionalString (m ? noremap) ", noremap = ${toString m.noremap}";
+      opts = concatStrings [ modeOpt prefixOpt buffer silent noremap ];
+    in
+    "{ ${renderString output}, ${renderString label}${opts} }";
+
+  render = i: km:
+    if km ? group
+    then renderGroup i km
+    else renderMapping km;
+
+  nkeymap = {
+    a = {
+      group = "Action";
+      c = { plug = "coc-codeaction-cursor"; label = "Near Cursor (LSP)"; };
+      f = { cmd = "CocCommand editor.action.formatDocument"; label = "Format (LSP)"; };
+      h = { cmd = "call CocActionAsync('doHover')"; label = "Hover (LSP)"; };
+      i = { cmd = "CocCommand editor.action.organizeImports"; label = "Organize Imports (LSP)"; };
+      l = { plug = "coc-codeaction-line"; label = "On Line (LSP)"; };
+      o = { plug = "coc-codelens-action"; label = "Code Lens (LSP)"; };
+      q = { plug = "coc-fix-current"; label = "Quick Fix (LSP)"; };
+      x = { cmd = "CocList commands"; label = "List LSP Commands"; };
+    };
+    b = {
+      group = "Buffers";
+      b = { cmd = "BufferPick"; label = "Pick"; };
+      "." = { cmd = "BufferPrevious"; label = "Previous"; };
+      "," = { cmd = "BufferNext"; label = "Next"; };
+      m = {
+        group = "Move ...";
+        "," = { cmd = "BufferMovePrevious"; label = "Previous"; };
+        "." = { cmd = "BufferMoveNext"; label = "Next"; };
+      };
+      "1-9" = "Go to buffer number";
+      "1" = { cmd = "BufferGoto 1"; hide = true; };
+      "2" = { cmd = "BufferGoto 2"; hide = true; };
+      "3" = { cmd = "BufferGoto 3"; hide = true; };
+      "4" = { cmd = "BufferGoto 4"; hide = true; };
+      "5" = { cmd = "BufferGoto 5"; hide = true; };
+      "6" = { cmd = "BufferGoto 6"; hide = true; };
+      "7" = { cmd = "BufferGoto 7"; hide = true; };
+      "8" = { cmd = "BufferGoto 8"; hide = true; };
+      "9" = { cmd = "BufferGoto 9"; hide = true; };
+      "0" = { cmd = "BufferLast"; label = "Last"; };
+      c = { cmd = "BufferClose"; label = "Close"; };
+      p = { cmd = "BufferPin"; label = "Pin"; };
+      o = {
+        group = "Order By ...";
+        n = { cmd = "BufferOrderByBufferNumber"; label = "Number"; };
+        l = { cmd = "BufferOrderByLanguage"; label = "Language"; };
+        d = { cmd = "BufferOrderByDirectory"; label = "Directory"; };
+        w = { cmd = "BufferOrderByWindowNumber"; label = "Window"; };
+      };
+    };
+    d = {
+      group = "Diagnostics";
+      l = { cmd = "CocList diagnostics"; label = "List (LSP)"; };
+      "." = { plug = "coc-diagnostic-next"; label = "Next (LSP)"; };
+      "," = { plug = "coc-diagnostic-prev"; label = "Previous (LSP)"; };
+    };
+    f = {
+      group = "Find";
+      n = { cmd = "NERDTreeFind"; label = "NERDTree"; };
+      r = { plug = "coc-references"; label = "References (LSP)"; };
+    };
+    g = {
+      group = "Go to...";
+      d = { plug = "coc-definition"; label = "Definition (LSP)"; };
+      i = { plug = "coc-implementation"; label = "Implementation (LSP)"; };
+      t = { plug = "coc-type-definition"; label = "Type Definition (LSP)"; };
+      n = { cmd = "NERDTreeFocus"; label = "NERDTree"; };
+      o = { cmd = "CocList outlint"; label = "Outline (LSP)"; };
+    };
+    r = {
+      group = "Refactor";
+      n = { plug = "coc-rename"; label = "Rename (LSP)"; };
+      r = { plug = "coc-refactor"; label = "Refactor (LSP)"; };
+    };
+    t = {
+      group = "Toggle";
+      n = { cmd = "NERDTreeToggle"; label = "NERDTree"; };
+    };
+  };
+
+  script = pkgs.writeText "which-key.lua" ''
+    local wk = require("which-key")
+
+    wk.setup {
+    }
+
+    wk.register(
+      ${renderGroup "  " nkeymap},
+      {
+        prefix = "<Leader>"
+      }
+    )
+  '';
+in
+{
+  config.programs.neovim = {
+    plugins = [
+      {
+        plugin = pkgs.vimPlugins.which-key-nvim;
+        config = ''
+          luafile ${script}
+        '';
+      }
+    ];
+  };
+}
